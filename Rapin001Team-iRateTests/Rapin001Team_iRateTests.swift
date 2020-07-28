22c165f5e13b63bbc4dfbd9b0f55d5aa395e8d46
RexAevum <rolans2k@gmil.com>
2020-07-27 20:20:31 -0400

    Initial Commit

---
+++ Rapin001Team-iRateTests/Rapin001Team_iRateTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rapin001Team_iRateTests.swift
+//  Rapin001Team-iRateTests
+//
+//  Created by Rolans Apinis on 7/27/20.
+//  Copyright © 2020 Rolans Apinis. All rights reserved.
+//
+
+import XCTest
+@testable import Rapin001Team_iRate
+
+class Rapin001Team_iRateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
